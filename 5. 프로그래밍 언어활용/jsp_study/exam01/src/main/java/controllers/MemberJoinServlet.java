25b5be6ae5d59b1abf06b455311e07bc8d326206
park198909 <dudrms1123@naver.com>
2023-05-04 17:53:12 +0900

    0504수업자료

---
+++ 5. 프로그래밍 언어활용/jsp_study/exam01/src/main/java/controllers/MemberJoinServlet.java
@@ -0,0 +1,29 @@
+package controllers;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+@WebServlet("/member/join") // 접속 URL
+public class MemberJoinServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String[] addCss= {"member/join","member/calendar"};
+        List<String> addScript = Arrays.asList("member/join","file/upload");
+
+        req.setAttribute("addCss",addCss);
+        req.setAttribute("addScript",addScript);
+
+        // JSP연동
+        RequestDispatcher rd = req.getRequestDispatcher("/member/join.jsp");
+        // 출력버퍼(화면) 구현
+        rd.forward(req,resp);
+    }
+}
---
+++ 5. 프로그래밍 언어활용/jsp_study/tdd/src/test/java/tests/MemberJoinTest.java
@@ -0,0 +1,76 @@
+package tests;
+
+import models.member.JoinService;
+import models.member.JoinValidationException;
+import models.member.JoinValidator;
+import models.member.Member;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class MemberJoinTest {
+
+    private JoinService service;
+    private Member member;
+
+    @BeforeEach
+    void init() {
+        service = new JoinService(new JoinValidator());
+        member = getMember();
+    }
+
+    private Member getMember() {
+        member = new Member();
+        member.setUserId("user01");
+        member.setUserPw("12345678");
+        member.setUserPwRe("12345678");
+        member.setUserNm("사용자01");
+        return member;
+    }
+
+    @Test
+    @DisplayName("회원가입 성공하면 예외발생 안함")
+    void joinSuccessTest() {    // 단위테스트1
+        assertDoesNotThrow(()->{
+            service.join(member);
+        });
+    }
+    
+    @Test
+    @DisplayName("필수항목(userId, userPw, userPwRe, userNm) 체크 - 검증 실패시 JoinValidationException 발생")
+    void joinRequiredFieldsTest() {
+        assertAll(()->{
+            // userId 필수항목 검증 - null
+            assertThrows(JoinValidationException.class,()->{
+                Member member = getMember();
+                member.setUserId(null);
+                service.join(member);
+            });
+            // userId 필수항목 검증 - 공백
+            assertThrows(JoinValidationException.class,()->{
+               Member member = getMember();
+               member.setUserId("   ");
+                service.join(member);
+            });
+            // userPw 필수항목 검증 - 공백
+            assertThrows(JoinValidationException.class,()->{
+                Member member = getMember();
+                member.setUserPw(null);
+                service.join(member);
+            });
+            // userPw 필수항목 검증 - 공백
+            assertThrows(JoinValidationException.class,()->{
+                Member member = getMember();
+                member.setUserPw("   ");
+                service.join(member);
+            });
+        });
+    }
+
+    
+}
