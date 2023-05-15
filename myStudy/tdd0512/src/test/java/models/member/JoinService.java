98709f7b0e76bfa33d9c5908a2d798368ee13c9e
park198909 <dudrms1123@naver.com>
2023-05-15 20:55:35 +0900

    no message

---
+++ myStudy/tdd0512/src/test/java/models/member/JoinService.java
@@ -0,0 +1,16 @@
+package models.member;
+
+public class JoinService {
+    private JoinValidator validator;
+    private MemberDao memberDao;
+    public JoinService(JoinValidator validator, MemberDao memberDao) {
+        this.validator = validator;
+        this.memberDao = memberDao;
+    }
+
+    public void join(Member member) {
+        validator.check(member);
+
+        memberDao.put(member);
+    }
+}
